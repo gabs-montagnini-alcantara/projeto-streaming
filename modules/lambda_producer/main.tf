94c68d2576cb1d2dd6f5cb3bee49cfa94947dec8
gabs-montagnini-alcantara <74217877+gabs-montagnini-alcantara@users.noreply.github.com>
2025-05-10 19:28:28 -0300

    Update lambda_function handler

--- modules/lambda_producer/main.tf
+++ modules/lambda_producer/main.tf
@@ -42,7 +42,7 @@ resource "aws_lambda_function" "producer" {
   function_name = var.lambda_name
   role          = aws_iam_role.lambda_role.arn
   runtime       = "python3.9"
-  handler       = "index.lambda_handler"
+  handler       = "lambda_function.lambda_handler"
   memory_size   = var.memory_size
   timeout       = var.timeout
   filename         = "${path.module}/lambda_function.zip"
